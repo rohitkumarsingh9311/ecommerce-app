86357bc55024c48970dac57841c08f938f3fa895
Rohit Kumar <rohitkumarsingh931@gmail.com>
2025-08-09 16:51:42 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,10 @@ connectCloudinary();
 
 
 //middle ware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
 
 app.use('/api/user',userRouter);
 app.use('/api/product',ProductRouter);
